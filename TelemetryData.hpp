eea3c4ac08a4a802779b680b0a33771222dc6d95
Taylor Fouts <fouts.taylor@gmail.com>
2025-04-18 09:50:49 -0400

    Add TelemetryData interface

---
+++ TelemetryData.hpp
@@ -0,0 +1,31 @@
+#ifndef TELEMETRYDATA_HPP
+#define TELEMETRYDATA_HPP
+
+#include <QObject>
+#include <QGeoCoordinate>
+
+class TelemetryData : public QObject
+{
+    Q_OBJECT
+    
+    Q_PROPERTY(int battery READ battery NOTIFY batteryChanged)
+    Q_PROPERTY(int altitude READ altitude NOTIFY altitudeChanged)
+    Q_PROPERTY(int speed READ speed NOTIFY speedChanged)
+    Q_PROPERTY(QGeoCoordinate position READ position NOTIFY positionChanged)
+    
+public:
+    virtual ~TelemetryData() = default;
+    
+    virtual int battery() const = 0;
+    virtual int altitude() const = 0;
+    virtual int speed() const = 0;
+    virtual QGeoCoordinate position() const = 0;
+    
+signals:
+    void batteryChanged(int battery);
+    void altitudeChanged(int altitude);
+    void speedChanged(int speed);
+    void positionChanged(QGeoCoordinate position);
+};
+
+#endif // TELEMETRYDATA_HPP
